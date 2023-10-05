dee377fe2324659a17be3fafb2c5f77155e7c9d4
AadityaUoHyd <aadiraj48@gmail.com>
2023-10-05 11:38:54 +0530

    springboot3 jdbc client code added

---
+++ src/main/java/org/aadi/jdbcClient/JdbcClientExampleApplication.java
@@ -0,0 +1,13 @@
+package org.aadi.jdbcClient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdbcClientExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JdbcClientExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/aadi/jdbcClient/JdbcClientExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package org.aadi.jdbcClient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdbcClientExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
